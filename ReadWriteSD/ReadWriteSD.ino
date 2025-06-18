f3f2aa8d1fc628f9a6fda158792afbffb5f76f02
Fx <felixrodlsberger@gmail.com>
2025-06-18 18:50:18 +0200

    writing works, but sometimes fragmented.

--- ReadWriteSD/ReadWriteSD.ino
+++ ReadWriteSD/ReadWriteSD.ino
@@ -1,54 +1,45 @@
-#include <SD.h>
+#include "sd_card_handler.h"
 
-const int chipSelect = A5;      // Passe ggf. an dein Setup an
-const int cardDetectPin = A4;   // Card Detect Pin
+SDCardHandler sdCard(A5, A4);
 
-File myFile;
+void writeCsvLine(File& file) {
+    float temperatur = 23.5;
+    int feuchtigkeit = 42;
+    const char* kommentar = "Hallo";
+    const char* datum = "2024-06-18";
+    file.print(temperatur, 1); file.print(",");
+    file.print(feuchtigkeit); file.print(",");
+    file.print(kommentar); file.print(",");
+    file.println(datum);
+}
 
 void setup() {
   Serial.begin(9600);
   delay(2000);
-  pinMode(cardDetectPin, INPUT);
-
-  int status = digitalRead(cardDetectPin);
-  if (status == LOW) {
-    Serial.println("Keine SD-Karte eingesteckt!");
-    while (true);
-  } else {
-    Serial.println("SD-Karte erkannt. Initialisiere...");
-  }
-
-  if (!SD.begin(chipSelect)) {
-    Serial.println("SD-Initialisierung fehlgeschlagen!");
-    while (true);
-  }
-  Serial.println("SD-Initialisierung erfolgreich.");
-
-  // Schreibversuch
-  myFile = SD.open("test.txt", FILE_WRITE);
-  if (myFile) {
-    Serial.print("Schreibe in test.txt...");
-    myFile.println("Hallo von der SD-Karte!");
-    myFile.close();
-    Serial.println("fertig.");
-  } else {
-    Serial.println("Fehler beim Öffnen von test.txt zum Schreiben!");
-  }
-
-  // Leseversuch
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("Inhalt von test.txt:");
-    while (myFile.available()) {
-      Serial.write(myFile.read());
+  Serial.println("DEBUG: Start setup()");
+  sdCard.begin();
+  Serial.println("DEBUG: Nach sdCard.begin()");
+  if (sdCard.isCardInserted()) {
+    Serial.println("DEBUG: Karte erkannt");
+    if (sdCard.init()) {
+      Serial.println("DEBUG: SD init OK");
+      sdCard.writeStringLine("daten.csv", "A,B,C,D");
+      delay(10);
+      sdCard.writeStringLine("daten.csv", "A,B,C,D");
+      delay(10);
+      sdCard.writeStringLine("daten.csv", "A,B,C,D");
+      delay(10);
+      sdCard.writeCustomLine("daten.csv", writeCsvLine);
+      delay(10);
+      sdCard.writeCustomLine("daten.csv", writeCsvLine);
+      delay(10);
+      sdCard.writeCustomLine("daten.csv", writeCsvLine);
+    } else {
+      Serial.println("DEBUG: SD init FEHLER");
     }
-    myFile.close();
-    Serial.println();
   } else {
-    Serial.println("Fehler beim Öffnen von test.txt zum Lesen!");
+    Serial.println("DEBUG: Keine Karte erkannt");
   }
 }
 
-void loop() {
-  // nichts passiert nach setup
-}
+void loop() {}
