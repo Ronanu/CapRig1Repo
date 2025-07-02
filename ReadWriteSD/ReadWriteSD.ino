d93d700edc89fa64ddbcfcd89da146a0a1a055e2
Fx <felixrodlsberger@gmail.com>
2025-07-02 19:40:01 +0200

    umsteg zu esp32

--- ReadWriteSD/ReadWriteSD.ino
+++ ReadWriteSD/ReadWriteSD.ino
@@ -1,6 +1,6 @@
 #include "sd_card_handler.h"
 
-SDCardHandler sdCard(A5, A4);
+SDCardHandler sdCard(10, A4);
 
 void writeCsvLine(File& file) {
     float temperatur = 23.5;
@@ -14,32 +14,94 @@ void writeCsvLine(File& file) {
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(2000);
   Serial.println("DEBUG: Start setup()");
-  sdCard.begin();
-  Serial.println("DEBUG: Nach sdCard.begin()");
-  if (sdCard.isCardInserted()) {
-    Serial.println("DEBUG: Karte erkannt");
-    if (sdCard.init()) {
-      Serial.println("DEBUG: SD init OK");
-      sdCard.writeStringLine("daten.csv", "A,B,C,D");
-      delay(10);
-      sdCard.writeStringLine("daten.csv", "A,B,C,D");
-      delay(10);
-      sdCard.writeStringLine("daten.csv", "A,B,C,D");
-      delay(10);
-      sdCard.writeCustomLine("daten.csv", writeCsvLine);
-      delay(10);
-      sdCard.writeCustomLine("daten.csv", writeCsvLine);
-      delay(10);
-      sdCard.writeCustomLine("daten.csv", writeCsvLine);
-    } else {
-      Serial.println("DEBUG: SD init FEHLER");
-    }
-  } else {
-    Serial.println("DEBUG: Keine Karte erkannt");
+  
+  // Direkte SD-Library Tests ohne Handler
+  Serial.println("=== DIREKTE SD TESTS ===");
+  
+  // Pin 10 konfigurieren
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  
+  Serial.println("Test 1: SD.begin(10) - Standard");
+  if (SD.begin(10)) {
+    Serial.println("SUCCESS: Standard funktioniert!");
+    testDirectSD();
+    return;
   }
+  Serial.println("Fehlgeschlagen");
+  
+  Serial.println("Test 2: SD.begin(10, SPI_HALF_SPEED)");
+  if (SD.begin(10, SPI_HALF_SPEED)) {
+    Serial.println("SUCCESS: Halbe Geschwindigkeit funktioniert!");
+    testDirectSD();
+    return;
+  }
+  Serial.println("Fehlgeschlagen");
+  
+  Serial.println("Test 3: SD.begin(10, SPI_QUARTER_SPEED)");
+  if (SD.begin(10, SPI_QUARTER_SPEED)) {
+    Serial.println("SUCCESS: Viertel Geschwindigkeit funktioniert!");
+    testDirectSD();
+    return;
+  }
+  Serial.println("Fehlgeschlagen");
+  
+  Serial.println("ALLE DIREKTEN TESTS FEHLGESCHLAGEN!");
+  Serial.println("Mögliche Ursachen:");
+  Serial.println("1. SD-Karte defekt/nicht kompatibel");
+  Serial.println("2. SD-Karte nicht richtig formatiert (FAT16/FAT32)");
+  Serial.println("3. Stromversorgung unzureichend");
+  Serial.println("4. SD-Karte zu alt/zu neu");
+  Serial.println("5. Kontakte verschmutzt");
+}
+
+void testDirectSD() {
+  Serial.println("\n=== SD DIREKT-TEST ===");
+  
+  // Schreibtest
+  File file = SD.open("direct.txt", FILE_WRITE);
+  if (file) {
+    file.println("Direkter Test erfolgreich!");
+    file.println("Zeitstempel: 2024-07-02");
+    file.close();
+    Serial.println("Schreiben: OK");
+  } else {
+    Serial.println("Schreiben: FEHLER");
+    return;
+  }
+  
+  // Lesetest
+  file = SD.open("direct.txt", FILE_READ);
+  if (file) {
+    Serial.println("Dateiinhalt:");
+    while (file.available()) {
+      Serial.write(file.read());
+    }
+    file.close();
+    Serial.println("Lesen: OK");
+  } else {
+    Serial.println("Lesen: FEHLER");
+  }
+  
+  Serial.println("\n=== DIREKT-TEST ERFOLGREICH ===");
+  Serial.println("SD-Karte funktioniert! Problem liegt am Handler.");
+}
+
+void printFileContents(const char* filename) {
+    File file = SD.open(filename, FILE_READ);
+    if (file) {
+        Serial.println("--- DATEIINHALT ---");
+        while (file.available()) {
+            Serial.write(file.read());
+        }
+        file.close();
+        Serial.println("\n--- ENDE ---");
+    } else {
+        Serial.println("FEHLER: Datei konnte nicht geöffnet werden!");
+    }
 }
 
 void loop() {}
